2c13eaf431a6c8e2e780349eb348d96482a3dfcf
Felix Prasanna <fpx@mit.edu>
2022-11-19 23:02:17 -0500

    formatting, cleanup

--- src/starter.rs
+++ src/starter.rs
@@ -2,13 +2,15 @@ use std::convert::From;
 use std::error::Error;
 use std::fs;
 use std::{fmt::Display, str::FromStr};
-
 use clap::{Parser, Subcommand};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Task::parse();
-    let tasks = fs::read_to_string("todos.txt")?;
-    let mut t = tasks.trim().parse::<Tasks>()?;
+
+    let todos = fs::read_to_string("todos.txt")?;
+
+    let mut t = todos.trim().parse::<Tasks>()?;
+
     // NOTE: MATCH, destructuring
     match args.command {
         Command::Delete { id } => t.0.retain(|task| task.id != id),
@@ -19,16 +21,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         }),
         Command::Display => println!("{}", t),
     }
+
     fs::write("todos.txt", t.to_string())?;
     Ok(())
 }
 
+// NOTE: CLI parsing
 #[derive(Parser)]
 struct Task {
     #[command(subcommand)]
     command: Command,
 }
 
+// NOTE: CLI parsing
 #[derive(Subcommand)]
 enum Command {
     Delete { id: usize },
@@ -37,27 +42,43 @@ enum Command {
     Display,
 }
 
+// NOTE: use of Vec<>
 #[derive(Debug)]
 struct Tasks(Vec<Todo>);
 
+// NOTE: common debugs
 #[derive(Parser, Clone, Debug)]
 struct Todo {
     task: String,
     id: usize,
 }
 
+
+////////////
+// Part 2 //
+////////////
+
+// NOTE: use of trait, fromstr for parsing
 impl FromStr for Tasks {
     type Err = &'static str;
 
+    // TODO: change to partition
+    // https://doc.rust-lang.org/rust-by-example/error/iter_result.html
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // NOTE: iterators
+        if s.trim().is_empty() {
+            return Ok(Tasks(vec![]));
+        }
         let (tasks, errors): (Vec<_>, Vec<_>) = s
-            .trim()
             .split('\n')
+            .into_iter()
             .map(str::parse::<Todo>)
             .partition(Result::is_ok);
 
-        for err in errors {
-            println!("{}", err.unwrap_err());
+        if !errors.is_empty() {
+            for err in errors {
+                println!("{}", err.unwrap_err());
+            }
         }
 
         if tasks.is_empty() {
@@ -70,14 +91,9 @@ impl FromStr for Tasks {
 
 impl Display for Tasks {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(
-            &self
-                .0
-                .iter()
-                .map(Todo::to_string)
-                .collect::<Vec<_>>()
-                .join("\n"),
-        )?;
+        for task in &self.0 {
+            f.write_str(&format!("{task}\n"))?
+        }
         Ok(())
     }
 }
@@ -87,20 +103,23 @@ impl FromStr for Todo {
 
     // format: id:task
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(':');
         // NOTE: question mark error handling
-        let (id, task) = s.split_once(':').ok_or("failed to split on :")?;
+        let id = split.next().ok_or("no id provided")?.parse::<usize>();
 
-        if let Ok(id) = id.parse::<usize>() {
+        // NOTE: if let, not explicitly returning
+        if let (Some(task), Ok(id)) = (split.next(), id) {
             Ok(Todo {
                 task: task.trim().to_owned(),
                 id,
             })
         } else {
-            Err("failed to parse id")
+            Err("got id but no task")
         }
     }
 }
 
+// NOTE: display
 impl Display for Todo {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!("{}: {}", self.id, self.task))
